134eaf3abe78ec650ee325354a37a62dc87e5930
ManuMontaraz <montarazmanu@gmail.com>
2026-05-11 23:36:43 +0200

    add ecosystem.config

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'leafpack',
+    script: './server.js',
+    instances: 1,
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '500M',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    },
+    error_file: './logs/err.log',
+    out_file: './logs/out.log',
+    merge_logs: true
+  }]
+}
